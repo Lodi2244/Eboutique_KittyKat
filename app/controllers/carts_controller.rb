4556e0932c642c368cdee8c41a80bd87ca28d052
casuis <rthrsmn@gmail.com>
2018-11-28 17:01:58 +0100

    not terminated met show car

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,13 +1,26 @@
 class CartsController < ApplicationController
 
-  def show
-  	@cart = Cart.find((User.find(current_user.id)).cart.id)
-    @total_cart = 0
-	@item = Item.all
-	@item.each do |item|
-	  if(item.carts.ids == @cart.id)
-	    @total_cart += item.price
+  def initialize
+  	if(current_user != nil)
+  	 @cart = Cart.find((User.find(current_user.id)).cart.id)
+	elsif(current_user)
+	  @cart = Cart.new(user_id: current_user.id)
+	  if(@cart.save)
+	    flash[:info] = "Connexion done"
+	    redirect_to root_url
+	  else
+	    flash[:info] = "Error please try again"
+	    #redirect_to
 	  end
+	else
+	  flash[:info] = "Error please try again"
+	  #redirect_to
 	end
+  end
+
+  def show
+  end
+
+  def method_name
   end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,14 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def total_price
+    @total_cart = 0
+	@items = Item.all
+	@items.each do |item|
+	  if(item.carts.ids == @cart.id)
+	    @total_cart += item.price
+	  end
+	end
+  end
 end
