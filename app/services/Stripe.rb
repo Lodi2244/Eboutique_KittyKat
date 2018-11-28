9efa76e869d9c08ad66013792c53d4d616a33922
Lodi2244 <elodieletellier1@gmail.com>
2018-11-28 19:38:45 +0100

    Stripe plugged

--- app/services/Stripe.rb
+++
@@ -1,8 +0,0 @@
-class Stripe
-
-  Rails.configuration.stripe = {
-    :publishable_key => Rails.application.credentials.STRIPE_PUBLISHABLE_KEY,
-    :secret_key => Rails.application.credentials.STRIPE_SECRET_KEY
-  }
-
-  Stripe.api_key = Rails.configuration.stripe[:publishable_key]
