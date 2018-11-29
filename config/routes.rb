5933a307d6585d9e64369578a964033726df69b4
Lodi2244 <elodieletellier1@gmail.com>
2018-11-29 22:19:43 +0100

    view User sign_in + sign_up

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   resources :charges, only: [:create, :new]
   resources :items, only: [:index, :show]
-  resources :users
-  get 'cart', to: 'product#show'
+  resource :cart, only: [:show]
   get 'add_to_cart', to: 'items#add_to_cart'
   devise_for :users
   root to: 'items#index'
