00a4222e8bfbda2804f7254e90524c1f4cadee2b
Lodi2244 <elodieletellier1@gmail.com>
2018-11-29 13:36:36 +0100

    test color

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  after_commit :send_welcome_mail, on: create
-  after_commit :send_profile_update_notification, on: update
-  after_commit :remove_profile_data, on: destroy
+  #after_commit :send_welcome_mail, on: create
+  #after_commit :send_profile_update_notification, on: update
+  #after_commit :remove_profile_data, on: destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one :cart
