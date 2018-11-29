3434025b5f1c021033c36d09d7941572689de7da
Lodi2244 <elodieletellier1@gmail.com>
2018-11-29 14:27:29 +0100

    test issue prod env

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
   default from: 'roxy.foxy2244@gmail.com'
 
   def welcome_email
-    @user = params[:user]
+    #@user = params[:user]
     @url  = 'http://example.com/login'
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
