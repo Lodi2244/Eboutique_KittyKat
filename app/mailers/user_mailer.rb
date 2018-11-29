dbb146d2bcc5f9547a05d124289d42b504baf7fd
Lodi2244 <elodieletellier1@gmail.com>
2018-11-29 14:28:49 +0100

    test2 issue prod env

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,9 +3,9 @@ class UserMailer < ApplicationMailer
 
   def welcome_email
     #@user = params[:user]
-    @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+    #@url  = 'http://example.com/login'
+    #mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
 end
 
-UserMailer.welcome_email.deliver
+#UserMailer.welcome_email.deliver
