f69a31c6c490f29fce2fe7d3291157016df0eaa9
Lodi2244 <elodieletellier1@gmail.com>
2018-11-29 14:07:29 +0100

    resolve conflict with origin

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = current_user
+  end
+
+  def new
+  end
+
+  def create
+   @user = User.new(params[:user])
+
+   respond_to do |format|
+     if @user.save
+       UserMailer.with(user: @user).welcome_email.deliver_now
+
+       format.html { redirect_to(@user, notice: 'User was successfully created.') }
+       format.json { render json: @user, status: :created, location: @user }
+     else
+       format.html { render action: 'new' }
+       format.json { render json: @user.errors, status: :unprocessable_entity }
+     end
+   end
+ end
+end
